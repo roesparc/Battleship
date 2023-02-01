63f97f0e5943662819002d8385d21c070e6a4436
Rodesar <rockoesp@hotmail.com>
2023-01-31 18:53:12 -0600

    Add attack method and tests for it

--- src/modules/logic/player.js
+++ src/modules/logic/player.js
@@ -15,15 +15,14 @@ const findRandomCoordinates = (enemyBoard) => {
     }
   }
 
-  return [randomX, randomY];
+  return { randomX, randomY };
 };
 
 const Player = (player) => {
   const attack = (enemyBoard, x, y) => {
     if (player === "bot") {
       const coordinates = findRandomCoordinates(enemyBoard);
-      const randomX = coordinates[0];
-      const randomY = coordinates[1];
+      const { randomX, randomY } = coordinates;
 
       enemyBoard.receiveAttack(randomX, randomY);
     } else {
@@ -31,7 +30,29 @@ const Player = (player) => {
     }
   };
 
-  return { attack };
+  const placeShip = (board, x, y, position) => {
+    if (player === "bot") {
+      const initialNumberOfShips = board.getShips().length;
+      let searchShipPosition = true;
+
+      while (searchShipPosition) {
+        const randomX = Math.floor(Math.random() * 10);
+        const randomY = Math.floor(Math.random() * 10);
+        const shipPosition =
+          Math.floor(Math.random() * 2) === 0 ? "horizontal" : "vertical";
+
+        board.placeShip(randomX, randomY, shipPosition);
+
+        if (board.getShips().length > initialNumberOfShips) {
+          searchShipPosition = false;
+        }
+      }
+    } else {
+      board.placeShip(x, y, position);
+    }
+  };
+
+  return { attack, placeShip };
 };
 
 export default Player;
--- src/modules/logic/player.test.js
+++ src/modules/logic/player.test.js
@@ -17,15 +17,13 @@ beforeEach(() => {
   bot = Player("bot");
 });
 
-describe("Test player", () => {
-  test("Attacks bot gameboard", () => {
+describe("Test attack method", () => {
+  test("Player attacks successfully bot gameboard", () => {
     player.attack(botGameboard, 0, 0);
     expect(botGameboard.getBoard()[0][0]).toBe("missed");
   });
-});
 
-describe("Test bot", () => {
-  test("Attacks player gameboard", () => {
+  test("Bot attacks successfully player gameboard", () => {
     const playerReceiveAttackMock = jest.fn();
     playerGameboard.receiveAttack = playerReceiveAttackMock;
 
@@ -33,8 +31,8 @@ describe("Test bot", () => {
     expect(playerReceiveAttackMock).toHaveBeenCalled();
   });
 
-  test("Does not attack the same coordinate twice", () => {
-    let successfulHits = 0;
+  test("Bot does not attack the same coordinate twice", () => {
+    let successfulAttacks = 0;
 
     for (let i = 0; i < 100; i += 1) {
       bot.attack(playerGameboard);
@@ -42,10 +40,28 @@ describe("Test bot", () => {
 
     for (let i = 0; i < 10; i += 1) {
       for (let j = 0; j < 10; j += 1) {
-        if (playerGameboard.getBoard()[i][j] === "missed") successfulHits += 1;
+        if (playerGameboard.getBoard()[i][j] === "missed")
+          successfulAttacks += 1;
       }
     }
 
-    expect(successfulHits).toBe(100);
+    expect(successfulAttacks).toBe(100);
+  });
+});
+
+describe("Test placeShip method", () => {
+  test("Player is able to place a ship in a certain coordenate", () => {
+    player.placeShip(playerGameboard, 4, 4, "vertical");
+    expect(playerGameboard.getBoard()[4][4].length).toBe(5);
+  });
+
+  test("Bot is able to place ships in random coordenates", () => {
+    bot.placeShip(botGameboard);
+    bot.placeShip(botGameboard);
+    bot.placeShip(botGameboard);
+    bot.placeShip(botGameboard);
+    bot.placeShip(botGameboard);
+
+    expect(botGameboard.getShips().length).toBe(5);
   });
 });
